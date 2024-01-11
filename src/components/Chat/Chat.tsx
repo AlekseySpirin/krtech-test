fafca884184afbcd34c00a574ec7e7b8a643e244
AlekseySpirin <spirin.mezhmun@yandex.ru>
2024-01-11 20:46:39 +0300

    add: Chat .scss

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,13 +1,20 @@
 import React, {FC, useEffect, useRef} from 'react';
-import {Box, Grid, Input, Stack, Typography} from "@mui/material";
+import {Grid, Input, Stack, Typography} from "@mui/material";
 import Divider from "@mui/material/Divider";
 import userChat from '../../store/UserChat'
 import {observer} from "mobx-react-lite";
 import UserStore from "../../store/UserStore";
 import {myId} from "../../utils/constants";
+import {cn} from "@bem-react/classname";
+import './Chat.scss'
+import AnimatedDots from "../Icon/AnimatedDots";
 
 
 const Chat: FC = observer(() => {
+
+
+  const chat = cn('Chat')
+  const messages = cn('Messages')
 
   const currentUser = UserStore.users.find(user => user.id === userChat.userChatId);
   const containerRef = useRef<HTMLDivElement>(null)
@@ -16,7 +23,7 @@ const Chat: FC = observer(() => {
     // Прокрутка вниз контейнера при монтировании компонента
     if (containerRef.current) {
       containerRef.current.scrollTop = containerRef.current.scrollHeight;
-          }
+    }
   }, [currentUser]);
 
 
@@ -24,141 +31,40 @@ const Chat: FC = observer(() => {
     return null; // Если пользователя не найдено, выходим из компонента
   }
   return (
-    <Grid direction={'column'} container width={'100%'}>
-      <Grid
-        sx={{
-          height: '70px',
-          width: '100%',
-          backgroundColor: '#FFFFFF',
-        }}
-        direction={'column'}
-        display={'flex'}
-        justifyContent={'center'}
-        alignItems={'center'}
-        item
-      >
-        <Typography variant={"h6"} sx={{
-          color: '#114475',
-
-          fontFamily: 'Roboto',
-          fontSize: '20px',
-          fontStyle: 'normal',
-          fontWeight: '700',
-          lineHeight: '20px',
-          letterSpacing: '0.4px',
-        }}>
+    <Grid className={chat()}
+          container>
+      <Grid className={chat('TitleWrapper')}
+            container>
+        <Typography className={chat('Title')}>
           {`${currentUser.first_name} ${currentUser.last_name}`}
         </Typography>
-        <Typography variant={"h6"}
-                    textAlign={'center'}
-                    sx={{
-                      color: '#2580DB',
-                      paddingTop: '7px',
-                      fontFamily: 'Roboto',
-                      fontSize: '14px',
-                      fontStyle: 'normal',
-                      fontWeight: '700',
-                      lineHeight: '20px',
-                      letterSpacing: '0.4px',
-                    }}>
-          {currentUser.isTyping ? 'Печатает...' : 'был(а) недавно'}
+        <Typography className={chat('Subtitle')}>
+          {currentUser.isTyping ?  <AnimatedDots /> : 'был(а) недавно'}
         </Typography>
       </Grid>
       <Divider/>
-      <Grid item xs
-            ref={containerRef}
-            sx={{
-        backgroundColor: '#E7ECF1',
-        width: '100%',
-        position: 'relative',
-        overflowY: 'scroll',
-        maxHeight: '100%',
-        '& ul': {padding: 0},
-        scrollbarWidth: 'auto', // Для Firefox
-        '&::-webkit-scrollbar': {
-        width: '8px', // Ширина полосы скролла
-      },
-        '&::-webkit-scrollbar-thumb': {
-        backgroundColor: '#11447580', // Цвет полосы скролла
-        borderRadius: '3px', // Скругление углов полосы скролла
-      },
-        '&::-webkit-scrollbar-track': {
-        backgroundColor: 'background.paper', // Фон трека скролла соответствует фону контейнера
-        borderRadius: '3px', // Скругление углов фона скролла
-      },
-        scrollbarColor: '#11447580 transparent', // Цвет полосы и фона скролла (для Firefox)
-      }}>
-        <Box display={'grid'} >
-          {currentUser.messages.map((message) => (
-            <Grid container
-                  sx={message.sender.id === myId ? {
-                      flexDirection: 'row-reverse',
-                      padding: '10px',
-
-
-                    }
-                    :
-                    {
-                      flexDirection: 'row',
-                      padding: '10px',
-
-
-                    }}
-                  key={message.id}>
-              <Grid item
-                    display={'flex'}
-                    direction={'row'}
-                    style={{
-                      maxWidth: '470px',
-                      display: 'inline-flex',
-                      backgroundColor: message.sender.id === myId ? '#267FDB' : '#FFF',
-                      borderRadius: message.sender.id === myId ? '16px 16px 4px 16px' : '16px 16px 16px 4px',
-                      color: message.sender.id === myId ? '#FFF' : '#000',
-                      padding: '8px 16px 6px 16px',
-                      marginLeft: message.sender.id === myId ? '20px' : '0',
-                      marginRight: message.sender.id === myId ? '0' : '20px',
-                      alignItems: 'flex-end',
-                      gap: '10px',
-                    }}
-              >
-                <Typography
-                  variant="body1"
-                  textAlign={'start'}
-                >
-                  {message.content}
-                </Typography>
-                <Box style={{
-                  display: 'flex',
-                  paddingTop: '8px',
-                  alignItems: 'flex-end',
-                  gap: '10px'
-                }}>
-                  <Typography style={{}}
-                              variant="body2"
-                              textAlign={'right'}
-                  >
-                    {message.time}
-                  </Typography>
-                </Box>
-              </Grid>
+      <Grid className={messages('Wrapper')} item xs
+            ref={containerRef}>
+        {currentUser.messages.map((message) => (
+          <Grid className={`${messages('ItemWrapper')} ${message.sender.id === myId ? 'user' : 'sender'}`}
+                container
+                key={message.id}>
+            <Grid className={`${messages('Item')} ${message.sender.id === myId ? 'user' : 'sender'}`}>
+              <Typography className={`${messages('Text')} ${message.sender.id === myId ? 'user' : 'sender'}`}>
+                {message.content}
+              </Typography>
+              <Typography className={`${messages('Time')} ${message.sender.id === myId ? 'user' : 'sender'}`}>
+                {message.time}
+              </Typography>
             </Grid>
-          ))}
-        </Box>
+          </Grid>
+        ))}
       </Grid>
       <Divider flexItem/>
-      <Stack justifyContent={'center'} alignItems={'center'} direction={'row'} minHeight={'54px'} width={'100%'} sx={{
-        backgroundColor: '#fff'
-      }}>
-        <Input disableUnderline
-               style={{
-                 width: '95%',
-                 paddingLeft: '20px',
-                 border: 'none',
-                 borderRadius: '15px',
-                 minHeight: '17px',
-                 background: '#E7ECF1',
-                 alignItems: 'center',
-               }}
+      <Stack className={messages('InputWrapper')}>
+        <Input className={messages('Input')}
+               name={'message'}
+               disableUnderline
                placeholder={'Написать сообщение...'}
         />
       </Stack>
